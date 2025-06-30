422dbc51ceee3503b9c4cdb29e2457bbf5b4d157
natsume-yasuyo <117988165+natsume-yasuyo@users.noreply.github.com>
2025-07-01 05:05:41 +0900

    php読み取るように修正

--- lolipop/createRoom.php
+++ lolipop/createRoom.php
@@ -1,4 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204); // No Content
+    exit;
+}
+
 // データベース接続情報
 $host = 'mysql324.phy.lolipop.lan';
 $dbname = 'LAA1648452-painting';
